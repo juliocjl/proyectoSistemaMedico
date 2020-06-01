f7d4894d4efe9f5c361cbd0d0f12f2f090f24d9e
Prueba <hola@gmail.com>
2020-05-31 19:45:10 -0500

    version 3

--- proyectoCripto/php/c_aes.php
+++ proyectoCripto/php/c_aes.php
@@ -3,12 +3,9 @@ function encrypt_aes($plaintext, $key, $iv){
         
         $cipher = "aes-128-cbc"; //tipo de cifrado utilizado
         
-        if (in_array( $cipher, openssl_get_cipher_methods()) )
-        {
+        if (in_array( $cipher, openssl_get_cipher_methods()) ){
             $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv);
             //el vector de inicializacion se genera al momento de registrar al usuario
-
-    
             return $ciphertext; //Se regresa una cadena cifrada.
         }
     }
--- proyectoCripto/php/d_aes.php
+++ proyectoCripto/php/d_aes.php
@@ -3,11 +3,9 @@ function decrypt_aes($ciphertext, $key, $iv){
         
         $cipher = "aes-128-cbc"; //tipo de cifrado utilizado
         
-        if (in_array( $cipher, openssl_get_cipher_methods()) )
-        {
+        if (in_array( $cipher, openssl_get_cipher_methods()) ){
             //el vector de inicializacion se genera al momento de registrar al usuario
             $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv);
-            
             return $original_plaintext; //Se regresa una cadena decifrada.
         }
     }
